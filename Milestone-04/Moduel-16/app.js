96936621db9b0e5db676f889b217e58d9ca3ee5d
Tusar <tusarahmed141@gmail.com>
2023-09-20 15:03:14 +0600

    So far last update

--- Milestone-04/Moduel-16/app.js
+++ Milestone-04/Moduel-16/app.js
@@ -4,3 +4,10 @@ let myName = 'Tusar' // Camel Case
 let MyName = 'Tusar' // Pascal Case
 let my_name = 'Tusar' // Snake Case
 
+let num1 = 0.1;
+let num2 = 0.2;
+let sum = (num1 * 100 + num2 * 100) / 100;
+// sum = sum.toFixed(1);
+
+// console.log(typeof sum);
+// console.log(sum);
