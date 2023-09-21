30d157993a272a08ef63e0b4f5687366702bf5ca
Tusar <tusarahmed141@gmail.com>
2023-09-21 20:14:20 +0600

    I am going to solve some problem so lets begin

--- Milestone-04/Moduel-16/app.js
+++ Milestone-04/Moduel-16/app.js
@@ -11,3 +11,11 @@ let sum = (num1 * 100 + num2 * 100) / 100;
 
 // console.log(typeof sum);
 // console.log(sum);
+
+const newArray = [2, 4, 7, 'Tusar', 'Rakib'];
+for (let i = 0; i < newArray.length; i++) {
+  console.log(newArray[i]);  
+}
+
+const pp = newArray.push();
+console.log(pp);
