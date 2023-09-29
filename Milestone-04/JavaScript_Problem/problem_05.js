b0e76a8654db5d64ccbd54c43b8e23a5a5c8b9df
Tusar <tusarahmed141@gmail.com>
2023-09-29 23:08:04 +0600

    So far last update

---
+++ Milestone-04/JavaScript_Problem/problem_05.js
@@ -0,0 +1,7 @@
+/**
+ * Problem 01 : You are given three numbers 13, 79, and 45. Write a program that will print the largest number using if-else
+ * Problem 02 : You are given a triangle with the sides 9,8,9. Write a program to check whether a triangle is isosceles or not using if-else
+ * Here Isosceles Means = two sides are equal
+ */
+
+const 
