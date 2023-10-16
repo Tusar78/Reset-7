2aced369e4e65b823b27bb9794cde0cd6dc45d77
Tusar <tusarahmed141@gmail.com>
2023-10-16 23:52:59 +0600

    Last update

--- Milestone-04/JavaScript_Problem/Loop/problem1.js
+++ Milestone-04/JavaScript_Problem/Loop/problem1.js
@@ -2,6 +2,16 @@
 //   console.log('Ajke Amar Mon Valo Na!', i);
 // }
 
-for (let i = 58; i <= 98; i++) {
-  console.log(i);  
-}
+// for (let i = 58; i <= 98; i++) {
+//   console.log(i);  
+// }
+
+// Even Number between 412 to 456
+// let i = 412;
+// while (i <= 456) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+//   i++;
+// }
+
