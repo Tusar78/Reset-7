90a409722f5e61e0a0337f08257f7d01948c836d
Tusar <tusarahmed141@gmail.com>
2024-04-14 22:58:43 +0600

    Solved another two problem

--- Milestone-04/Assignment_04/app.js
+++ Milestone-04/Assignment_04/app.js
@@ -86,13 +86,57 @@ Task:
  * 
  */
 
-function findAddress(obj) {
-  
+// function findAddress(obj) {
+//   let myObj = {
+//     street: "__",
+//     house: "__",
+//     society: "__",
+//   };
+
+//   let address = `${obj.street ? obj.street : myObj.street},${
+//     obj.house ? obj.house : myObj.house
+//   },${obj.society ? obj.society : myObj.society}`;
+
+//   return address
+// }
+
+// const result = findAddress({
+//   street: 10,
+//   house: "15A",
+//   society: "Earth Perfect",
+// });
+// console.log(result);
+
+
+/**
+ * Problem - 5
+তুমি দ োকানে একটা চি প্স কি নতে গে লা কি ছুখুচরা টাকার ন োট নি য়ে । একটা canPay() নামে function বানাও যে টা
+২টা ইনপুট নে ইঃ একটা array, এবং আরে কটা number।
+1. Array এর প্রত্যে ক টা element ত োমার খুচরা টাকা বঝু াই
+1 taka
+2 taka
+5 taka
+2. second input টা বঝু াই চি প্স এর দাম।
+Task:
+1. যদি খুচরা টাকা গুল ো য োগ করে চি প্স এর দামে র সমান বা তার বে শি হয় তাহলে boolean true রি টার্ন
+করবে
+2. আর যদি ন োটগুল োর য োগফল চি প্স এর দামে র চে য়ে কম হয় সে ক্ষে ত্রে boolean false রি টার্ন করবে ।
+3. Bonus: ইনপুট এর প্রথম উপাদান empty অ্যারে কি না চে ক করবে । empty অ্যারে হলে মি নি ংফুল মে সে জ
+রি টার্ন করে দি বে ।
+ */
+
+function canPay(changeArray, totalDue) {
+    const productPrice = totalDue;
+    let totalMoney = 0;
+    for (const taka of changeArray) {
+        totalMoney += taka;
+    }
+
+    if (totalMoney === productPrice) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-const result = findAddress({
-  street: 10,
-  house: "15A",
-  society: "Earth Perfect",
-});
-console.log(result);
+const result = canPay([1, 2, 5], 10);
