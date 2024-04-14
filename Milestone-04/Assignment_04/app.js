b87ba1aa67eb91b200c9d83030bbd739049bba0c
Tusar <tusarahmed141@gmail.com>
2024-04-14 15:54:19 +0600

    Solve this find address problem in another way

--- Milestone-04/Assignment_04/app.js
+++ Milestone-04/Assignment_04/app.js
@@ -6,47 +6,93 @@ Task:
 করে দি বে ফাংশন থে কে ।
  */
 
+// function cubeNumber(number) {
+//     let result;
+//     if (typeof number !== 'number' || number < 1) {
+//         result = 'Please Enter the valid number!';
+//     } else {
+//         result = number * number * number;
+//     }
 
-function cubeNumber(number) {
-    let result;
-    if (typeof number !== 'number' || number < 1) {
-        result = 'Please Enter the valid number!';
-    } else {
-        result = number * number * number;
-    }
+//     return result;
+// }
 
-    return result;
-}
+// const result1 = cubeNumber(5)
+// const result2 = cubeNumber(4)
+// console.log(result1);
+// console.log(result2);
 
-const result1 = cubeNumber(5)
-const result2 = cubeNumber(4)
-console.log(result1);
-console.log(result2);
+// /**
+//  * 1. ত োমাকে একটা function দে ওয়া হবে called “matchFinder()” যা ইনপুট হি সে বে দইুটা string নি বে ।
+// Task:
+// 1. যদি প্রথম স্ট্রি ংটির ক োন ো একটি অংশে র সাথে দ্বি তীয় স্ট্রি ংটির পুর োপুরি মি ল খুজে পাও সে ক্ষে ত্রে তুমি
+// boolean রি টার্ন করবে এক্ষে ত্রে ত োমাকে true রি টার্ন করতে হবে ।
+// 2. আর যদি পুর োপুরি মি ল খুজে না পাও সে ক্ষে ত্রে তুমি boolean false রি টার্ন করবে ।
+// 3. Bonus: দটিু ইনপুট স্ট্রি ং দে ওয়া হল ো কি না সে টা validate করবে । যদি দইুটি বা যে ক োন ো একটি ইনপুট স্ট্রি ং
+// না হয় সে ক্ষে ত্রে তুমি একটা মি নি ংফুল মে সে জ রি টার্ন করে দি বে ।
+//  */
 
+// function matchFinder(string1, string2) {
+//     if (typeof string1 !== 'string' || typeof string2 !== 'string') {
+//         return 'Please Enter String Data!'
+//     } else {
+//         if (string1.includes(string2)) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     }
+
+// }
+
+// const result = matchFinder('Hello World', 'World');
+// console.log(result);
 
 /**
- * 1. ত োমাকে একটা function দে ওয়া হবে called “matchFinder()” যা ইনপুট হি সে বে দইুটা string নি বে ।
+ * Problem - 3
+ত োমাকে একটা ফাংশন লি খতে হবে যার নাম হবে sortMaker()। এই ফাংশনে র parameter হবে একটি array এবং
+এই array তে সবসময় দইুটি উপাদান থাকবে ।
 Task:
-1. যদি প্রথম স্ট্রি ংটির ক োন ো একটি অংশে র সাথে দ্বি তীয় স্ট্রি ংটির পুর োপুরি মি ল খুজে পাও সে ক্ষে ত্রে তুমি
-boolean রি টার্ন করবে এক্ষে ত্রে ত োমাকে true রি টার্ন করতে হবে ।
-2. আর যদি পুর োপুরি মি ল খুজে না পাও সে ক্ষে ত্রে তুমি boolean false রি টার্ন করবে ।
-3. Bonus: দটিু ইনপুট স্ট্রি ং দে ওয়া হল ো কি না সে টা validate করবে । যদি দইুটি বা যে ক োন ো একটি ইনপুট স্ট্রি ং
-না হয় সে ক্ষে ত্রে তুমি একটা মি নি ংফুল মে সে জ রি টার্ন করে দি বে ।
+1. যদি অ্যারে র দইুটি উপাদান পজি টিভ সংখ্যা হয় সে ক্ষে ত্রে তুমি অ্যারে টিকে বড় ো থে কে ছ োট ক্রমে সাজি য়ে রি টার্ন
+করবে ।
+2. যদি দইুটি উপাদান একই হয় সে ক্ষে ত্রে তুমি এই স্ট্রি ং রি টার্ন করবে ঃ “equal”
+3. Bonus: যদি অ্যারে র যে ক োন ো একটি উপাদান নে গে টিভ সংখ্যা হয় সে ক্ষে ত্রে তুমি রি টার্ন করবে “Invalid
+Input”
+ * 
  */
 
-function matchFinder(string1, string2) {
-    if (typeof string1 !== 'string' || typeof string2 !== 'string') {
-        return 'Please Enter String Data!'
-    } else {
-        if (string1.includes(string2)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+// function sortMaker(arr) {
+//     for (const ar of arr) {
+//         if (ar < 0) {
+//             return 'Invalid input'
+//         }
+//     }
 
-   
+//     const sortArr = arr.sort((a, b) => a - b);
+//     return sortArr
+// }
+
+// const mySort = sortMaker([120, 44]);
+// console.log(mySort);
+
+/**
+ * Problem - 4
+ত োমাকে একটা function দে ওয়া হবে called “findAddress()” যা ইনপুট হি সে বে একটা object নি বে । Object এর
+তি নটি property থাকবে ।
+Task:
+1. ত োমাকে ১ম sample output এর format এ output return করতে হবে ।
+2. Bonus: যদি object এর ক োন ো property missing থাকে সে ক্ষে ত্রে সে ই অংশটুকু double underscore
+দি য়ে replace হবে । (২য় output এর format এ )
+ * 
+ */
+
+function findAddress(obj) {
+  
 }
 
-const result = matchFinder('Hello World', 'World');
+const result = findAddress({
+  street: 10,
+  house: "15A",
+  society: "Earth Perfect",
+});
 console.log(result);
