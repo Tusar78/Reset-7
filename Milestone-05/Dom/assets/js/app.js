32d69ee968e4d7edcde2e92d944d7e09a6babcab
Tusar <tusarahmed141@gmail.com>
2024-04-25 11:47:07 +0600

    Practice dom navigator

--- Milestone-05/Dom/assets/js/app.js
+++ Milestone-05/Dom/assets/js/app.js
@@ -15,3 +15,21 @@ if (box.children) {
 } else {
   console.log('world');
 }
+
+const myTable = document.getElementById("myTable");
+
+// const cell = myTable.children[0].children;
+
+// for (let i = 0; i < cell.length; i++) {
+//     for (let j = 0; j <= i; j++) {
+//         console.log(cell[i].children[i]);
+//         cell[i].children[i].style.backgroundColor = 'red';
+//         cell[i].children[i].style.color = 'white';
+//     }
+// }
+
+// Another way
+const row = myTable.rows;
+for (let i = 0; i < row.length; i++) {
+    row[i].cells[i].style.backgroundColor = 'red'
+}
