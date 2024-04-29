e8b4b3bc1456614b3710a56091d66028b6d952f0
Tusar <tusarahmed141@gmail.com>
2024-04-29 16:00:59 +0600

    Prepare to practice dom event

---
+++ Milestone-05/Dom/Tree_From_Obj/app.js
@@ -0,0 +1,19 @@
+let data = {
+    "Fish": {
+      "trout": {},
+      "salmon": {}
+    },
+  
+    "Tree": {
+      "Huge": {
+        "sequoia": {},
+        "oak": {}
+      },
+      "Flowering": {
+        "apple tree": {},
+        "magnolia": {}
+      }
+    }
+  };
+
+  console.log(data);
