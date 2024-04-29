bc6221de148dc3e83aea797bfb2fb79e57c52a9f
Tusar <tusarahmed141@gmail.com>
2024-04-29 16:50:40 +0600

    Exploring awesome event

--- Milestone-05/Moduel_25/assets/js/app.js
+++ Milestone-05/Moduel_25/assets/js/app.js
@@ -1,23 +1,37 @@
 // Option 02
-const makeRed = () => {
-    document.body.style.backgroundColor = 'red';
-}
+// const makeRed = () => {
+//     document.body.style.backgroundColor = 'red';
+// }
 
-const makeYellow = document.querySelector('.make-yellow');
-const makePurple = document.querySelector('.make-purple');
-const makeGolden = document.querySelector('.make-golden');
+// const makeYellow = document.querySelector('.make-yellow');
+// const makePurple = document.querySelector('.make-purple');
+// const makeGolden = document.querySelector('.make-golden');
 
-// Option 03
-makeYellow.onclick = function() {
-    document.body.style.backgroundColor = 'yellow';
-}
+// // Option 03
+// makeYellow.onclick = function() {
+//     document.body.style.backgroundColor = 'yellow';
+// }
 
-// Option 04
-makePurple.addEventListener('click', () => {
-    document.body.style.backgroundColor = 'purple';
+// // Option 04
+// makePurple.addEventListener('click', () => {
+//     document.body.style.backgroundColor = 'purple';
+// })
+
+// // Option 05
+// document.querySelector('.make-golden').addEventListener('click',  function () {
+//     document.body.style.backgroundColor = 'goldenrod';
+// })
+
+const input = document.querySelector('input[id=name]');
+const printName = document.getElementById('printName');
+const displayName = document.querySelector('.display-name');
+
+printName.addEventListener('click', () => {
+    let inputVal = input.value;
+    displayName.textContent = inputVal;
 })
 
-// Option 05
-document.querySelector('.make-golden').addEventListener('click',  function () {
-    document.body.style.backgroundColor = 'goldenrod';
+input.addEventListener('keyup', (e) => {
+    let inputVal = e.target.value;
+    displayName.textContent = inputVal;
 })
