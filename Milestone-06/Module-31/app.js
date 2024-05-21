d08c37302dc571d7abbe655d6d80ffea8d7156b9
Tusar <tusarahmed141@gmail.com>
2024-05-21 06:37:35 +0600

    Bracket notation is better than dot notatio

--- Milestone-06/Module-31/app.js
+++ Milestone-06/Module-31/app.js
@@ -1,13 +1,33 @@
-class Teachers {
-    constructor(name, sub) {
-        this.name = name;
-        this.sub = sub;
-    }
+// class Teachers {
+//     constructor(name, sub) {
+//         this.name = name;
+//         this.sub = sub;
+//     }
 
-    dept() {
-        console.log(`I Love this ${this.sub}`);
-    }
+//     dept() {
+//         console.log(`I Love this ${this.sub}`);
+//     }
+// }
+
+// class subTeacher extends Teachers {
+//     constructor(name, sub, age) {
+//         super(name, sub);
+//         this.name = name;
+//         this.sub = sub;
+//         this.age = age;
+//     }
+// }
+
+// const tusar = new subTeacher("Tusar", "Web Development", 24);
+// console.log(tusar);
+// tusar.dept();
+
+
+const person = {
+    name : "tusar",
+    age: 23,
+    3: 'Rakib'
 }
 
-const tusar = new Teachers("Tusar", "Web");
-console.log(tusar);
+const my = 'name';
+console.log(person[my]);
