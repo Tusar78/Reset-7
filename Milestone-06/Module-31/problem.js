89e5b4ed397773b1ccdf08466418ed4800a38150
Tusar <tusarahmed141@gmail.com>
2024-05-21 07:04:43 +0600

    solve 3rd problem

--- Milestone-06/Module-31/problem.js
+++ Milestone-06/Module-31/problem.js
@@ -1,4 +1,22 @@
 // Problem 01
 const oddArray = [1, 3, 5, 7, 9];
-const evenArray = oddArray.map(elem => elem+1);
+const evenArray = oddArray.map((elem) => elem + 1);
 console.log(evenArray);
+
+// Problem 02
+const randomArr = [33, 50, 79, 78, 90, 101, 30];
+const diviseableTen = randomArr.filter((elem) => elem % 10 === 0);
+console.log(diviseableTen);
+// same task using find
+const diviseableTen2 = randomArr.find((elem) => elem % 10 === 0);
+console.log(diviseableTen2);
+
+// Problem 03
+const instructor = [
+  { name: "Aopo", age: 23, position: "Senior" },
+  { name: "Jannat", age: 22, position: "Junior" },
+  { name: "Salman", age: 23, position: "Senior" },
+];
+
+const seniors = instructor.filter(elem => elem.position === 'Senior');
+console.log(seniors);
