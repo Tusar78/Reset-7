472cd406f357cc10bbd6b19f76e824908b2cf701
Tusar <tusarahmed141@gmail.com>
2024-05-21 07:12:51 +0600

    Solve age sum function

--- Milestone-06/Module-31/problem.js
+++ Milestone-06/Module-31/problem.js
@@ -18,5 +18,26 @@ const instructor = [
   { name: "Salman", age: 23, position: "Senior" },
 ];
 
-const seniors = instructor.filter(elem => elem.position === 'Senior');
+const seniors = instructor.filter((elem) => elem.position === "Senior");
 console.log(seniors);
+
+// Problem 04
+const people = [
+  { name: "Meena", age: 20 },
+  { name: "Rina", age: 15 },
+  { name: "Suchorita", age: 22 },
+];
+
+// Solve using for loop
+let ageSum = 0;
+for (let i = 0; i < people.length; i++) {
+    ageSum += people[i].age;
+}
+
+// console.log(ageSum);
+
+const ageSum2 = people.reduce((acc, elem) => {
+    return acc + elem.age;
+}, 0)
+
+console.log(ageSum2);
