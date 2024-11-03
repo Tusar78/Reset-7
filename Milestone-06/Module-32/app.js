474724711e3ad7bb6d8a72fbf0c4e76daaa68b1a
Tusar <tusarahmed141@gmail.com>
2024-11-04 01:08:58 +0600

    Create new file

--- Milestone-06/Module-32/app.js
+++ Milestone-06/Module-32/app.js
@@ -1,16 +1,25 @@
-function loadData() {
-  const JSONLINK = "https://jsonplaceholder.typicode.com/users";
-  fetch(JSONLINK)
-    .then((response) => response.json())
-    .then((data) => dosplayData(data));
+// function loadData() {
+//   const JSONLINK = "https://jsonplaceholder.typicode.com/users";
+//   fetch(JSONLINK)
+//     .then((response) => response.json())
+//     .then((data) => dosplayData(data));
+// }
+
+// const dosplayData = (data) => {
+//   const ul = document.createElement('ul');
+//   for (const d of data) {
+//     let li =document.createElement('li');
+//     li.innerHTML= `${d.name}`;
+//     ul.append(li);
+//   }
+//   document.body.append(ul);
+// };
+
+
+const loadPhone = async() => {
+  const res = await fetch("https://openapi.programming-hero.com/api/phones?search=iphone");
+  const data = await res.json();
+  console.log(data.data);
 }
 
-const dosplayData = (data) => {
-  const ul = document.createElement('ul');
-  for (const d of data) {
-    let li =document.createElement('li');
-    li.innerHTML= `${d.name}`;
-    ul.append(li);
-  }
-  document.body.append(ul);
-};
+loadPhone();
